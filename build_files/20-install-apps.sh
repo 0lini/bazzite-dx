13fde9603b69c5396537f4c3eb8c46c6d9496090
0lini <72492483+0lini@users.noreply.github.com>
2026-06-07 18:49:53 +0200

    fix: switch VSCode repo to upstream repofile

--- build_files/20-install-apps.sh
+++ build_files/20-install-apps.sh
@@ -47,12 +47,9 @@ dnf5 install --enable-repo="copr:copr.fedorainfracloud.org:ublue-os:packages" -y
 # over using random coprs. Please keep this in mind when adding external dependencies.
 # If adding any dependency, make sure to always have it disabled by default and _only_ enable it on `dnf install`
 
-dnf5 config-manager addrepo --set=baseurl="https://packages.microsoft.com/yumrepos/vscode" --id="vscode"
-dnf5 config-manager setopt vscode.enabled=0
-# FIXME: gpgcheck is broken for vscode due to it using `asc` for checking
-# seems to be broken on newer rpm security policies.
-dnf5 config-manager setopt vscode.gpgcheck=0
-dnf5 install --nogpgcheck --enable-repo="vscode" -y \
+dnf5 config-manager addrepo --from-repofile="https://packages.microsoft.com/yumrepos/vscode/config.repo"
+dnf5 config-manager setopt vscode-yum.enabled=0
+dnf5 install --enable-repo="vscode-yum" -y \
     code
 
 docker_pkgs=(
