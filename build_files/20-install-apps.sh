7b0be3db551782a40f1b2549df41124c527c4b36
0lini <72492483+0lini@users.noreply.github.com>
2026-06-08 22:07:30 +0200

    fix: clean up install script for F44

--- build_files/20-install-apps.sh
+++ build_files/20-install-apps.sh
@@ -22,61 +22,23 @@ dnf5 install -y \
     waypipe \
     zsh
 
-dnf5 remove -y \
-    mesa-libOpenCL
+if [[ "$IMAGE_NAME" != *nvidia* ]]; then
+    dnf5 remove -y \
+        mesa-libOpenCL
+    dnf5 --setopt=install_weak_deps=False install -y \
+        rocm-hip \
+        rocm-opencl \
+        rocm-clinfo \
+        rocm-smi
+fi
 
 dnf5 --setopt=install_weak_deps=False install -y \
-    rocm-hip \
-    rocm-opencl \
-    rocm-clinfo \
-    rocm-smi \
     qemu \
     libvirt \
     qemu-kvm \
     virt-manager \
     edk2-ovmf \
     guestfs-tools
-
-# Restore UUPD update timer and Input Remapper
-sed -i 's@^NoDisplay=true@NoDisplay=false@' /usr/share/applications/input-remapper-gtk.desktop
-systemctl enable input-remapper.service
-systemctl enable uupd.timer
-
-# Remove -deck specific changes to allow for login screens and session selection in settings
-rm -f /etc/sddm.conf.d/steamos.conf
-rm -f /etc/sddm.conf.d/virtualkbd.conf
-rm -f /etc/sddm.conf.d/zz-steamos-autologin.conf
-rm -f /usr/share/gamescope-session-plus/bootstrap_steam.tar.gz
-dnf5 remove -y steamos-manager-powerstation
-dnf5 install --enable-repo="copr:copr.fedorainfracloud.org:ublue-os:bazzite" -y \
-    ds-inhibit
-systemctl enable ds-inhibit.service
-
-if [[ "$IMAGE_NAME" == *gnome* ]]; then
-    # Remove SDDM and re-enable GDM on GNOME builds.
-    dnf5 remove -y \
-        sddm
-
-    systemctl enable gdm.service
-else
-    dnf5 install -y \
-        plasma-login-manager
-
-    dnf5 remove -y \
-        sddm
-
-    systemctl enable plasmalogin
-
-    # Re-enable logout and switch user functionality in KDE
-    sed -i -E \
-      -e 's/^(action\/switch_user)=false/\1=true/' \
-      -e 's/^(action\/start_new_session)=false/\1=true/' \
-      -e 's/^(action\/lock_screen)=false/\1=true/' \
-      -e 's/^(kcm_sddm\.desktop)=false/\1=true/' \
-      -e 's/^(kcm_plymouth\.desktop)=false/\1=true/' \
-      /etc/xdg/kdeglobals
-fi
-
 
 dnf5 install --enable-repo="copr:copr.fedorainfracloud.org:ublue-os:packages" -y \
     ublue-setup-services
@@ -85,12 +47,9 @@ dnf5 install --enable-repo="copr:copr.fedorainfracloud.org:ublue-os:packages" -y
 # over using random coprs. Please keep this in mind when adding external dependencies.
 # If adding any dependency, make sure to always have it disabled by default and _only_ enable it on `dnf install`
 
-dnf5 config-manager addrepo --set=baseurl="https://packages.microsoft.com/yumrepos/vscode" --id="vscode"
-dnf5 config-manager setopt vscode.enabled=0
-# FIXME: gpgcheck is broken for vscode due to it using `asc` for checking
-# seems to be broken on newer rpm security policies.
-dnf5 config-manager setopt vscode.gpgcheck=0
-dnf5 install --nogpgcheck --enable-repo="vscode" -y \
+dnf5 config-manager addrepo --from-repofile="https://packages.microsoft.com/yumrepos/vscode/config.repo"
+dnf5 config-manager setopt vscode-yum.enabled=0
+dnf5 install --enable-repo="vscode-yum" -y \
     code
 
 docker_pkgs=(
@@ -102,13 +61,7 @@ docker_pkgs=(
 )
 dnf5 config-manager addrepo --from-repofile="https://download.docker.com/linux/fedora/docker-ce.repo"
 dnf5 config-manager setopt docker-ce-stable.enabled=0
-dnf5 install -y --enable-repo="docker-ce-stable" "${docker_pkgs[@]}" || {
-    # Use test packages if docker pkgs is not available for f42
-    if (($(lsb_release -sr) == 42)); then
-        echo "::info::Missing docker packages in f42, falling back to test repos..."
-        dnf5 install -y --enablerepo="docker-ce-test" "${docker_pkgs[@]}"
-    fi
-}
+dnf5 install -y --enable-repo="docker-ce-stable" "${docker_pkgs[@]}"
 
 # Load iptable_nat module for docker-in-docker.
 # See:
